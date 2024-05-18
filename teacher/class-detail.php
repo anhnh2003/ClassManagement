863cc7eb42b158fc4ee1327adc793bb18d19e9df
anhnh2003 <anh.nhcoder@gmail.com>
2024-05-18 23:37:25 +0700

    add secure

--- teacher/class-detail.php
+++ teacher/class-detail.php
@@ -3,9 +3,30 @@ session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
 
-if (strlen($_SESSION['sturecmsuid']) == 0) {
+// Check if the user is logged in and the session variables are set
+if (strlen($_SESSION['sturecmsstuid']) == 0) {
+
   header('location:logout.php');
+  exit();
 } else {
+  // Retrieve the 'uid' and 'session_token' cookies
+  $uid = $_COOKIE['uid'] ?? '';
+  $sessionToken = $_COOKIE['session_token'] ?? '';
+  // Prepare the SQL statement to select the token from the database
+  $sql = "SELECT UserToken, role_id FROM tbltoken WHERE UserID = :uid AND UserToken = :sessionToken AND (CreationTime + INTERVAL 2 HOUR) >= NOW()";
+  $query = $dbh->prepare($sql);
+  $query->bindParam(':uid', $uid, PDO::PARAM_INT);
+  $query->bindParam(':sessionToken', $sessionToken, PDO::PARAM_STR);
+  $query->execute();
+  $role_id = $query->fetch(PDO::FETCH_OBJ)->role_id;
+  // Check if the token exists and is not expired
+  if (($query->rowCount() == 0) || ($role_id != 2)) {
+      // Token is invalid or expired, redirect to logout
+      header('location:logout.php');
+      exit();
+
+  } else {
+    // Token is valid, continue
   if (isset($_POST['submit'])) {
     // $teaid = $_POST['teaid'];
     $cname = $_POST['cname'];
@@ -49,7 +70,7 @@ if (strlen($_SESSION['sturecmsuid']) == 0) {
     $query->execute();
     echo '<script>alert("Join Code has been changed")</script>';
   }
-}
+}}
 ?>
 
 <!DOCTYPE html>
