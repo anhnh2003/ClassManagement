81f0a1292a4f01c161ffb9acdd76c27483f5951c
Hwi <84950295+qhwi@users.noreply.github.com>
2024-05-19 02:36:57 +0700

    Update student/class-detail

--- student/class-detail.php
+++ student/class-detail.php
@@ -3,80 +3,20 @@ session_start();
 //error_reporting(0);
 include('includes/dbconnection.php');
 
-function getRandomStringShuffle($length = 43)
-{
-    $stringSpace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $stringLength = strlen($stringSpace);
-    $string = str_repeat($stringSpace, ceil($length / $stringLength));
-    $shuffledString = str_shuffle($string);
-    $randomString = substr($shuffledString, 1, $length);
-    return $randomString;
-}
-
 if (strlen($_SESSION['sturecmsuid']) == 0) {
   header('location:logout.php');
 } else {
-  if (isset($_POST['genqr'])) {
-    $aid = $_POST['attendance_id'];
-
-    // Generate QR code
-    include_once('../phpqrcode/qrlib.php');
-    $tempDir = 'temp/';
-
-    $qrContent = getRandomStringShuffle();
-    $qrImgName = "qrImg.png";
-    date_default_timezone_set('Asia/Ho_Chi_Minh');
-    $gentime = date('Y-m-d H:i:s');
-    $sql = "UPDATE tblattendance SET Secret=:qrContent, LastGeneratedTime=:gentime WHERE ID=:aid";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':aid', $aid, PDO::PARAM_STR);
-    $query->bindParam(':qrContent', $qrContent, PDO::PARAM_STR);
-    $query->bindParam(':gentime', $gentime, PDO::PARAM_STR);
-    $query->execute();
-    $pngAbsoluteFilePath = $tempDir.$qrImgName;
-    QRcode::png($qrContent, $pngAbsoluteFilePath, QR_ECLEVEL_L, 10, 10);
-    // echo "<div style='display: flex; justify-content: center; align-items: center; height: 100vh;'>";
-    // echo "<img src='".$pngAbsoluteFilePath."'>";
-    // echo "</div>";
-    // echo "<div style='display: flex; justify-content: center; align-items: center; height: 100vh;'>";
-    // echo "<br>Using shuffle(): " . getRandomStringShuffle();
-    // echo "</div>";
-    echo "<script>window.open('".$pngAbsoluteFilePath."');</script>";
-  }
-
-  if (isset($_POST['regencode'])) {
-    $characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    $joincode = '';
-    for ($i = 0; $i < 6; $i++) {
-      $index = rand(0, strlen($characters) - 1);
-      $joincode .= $characters[$index];
-    }
+  if (isset($_POST['leave'])) {
     $eid = $_GET['editid'];
-    $sql = "UPDATE tblclass SET JoinCode=:joincode WHERE ID=:eid";
+    $uid = $_SESSION['sturecmsuid'];
+    $sql = "DELETE FROM tblstudent_class WHERE student_id=:uid AND class_id=:eid";
     $query = $dbh->prepare($sql);
-    $query->bindParam(':joincode', $joincode, PDO::PARAM_STR);
-    $query->bindParam(':eid', $eid, PDO::PARAM_STR);
+    $query->bindParam(':eid',$eid,PDO::PARAM_STR);
+    $query->bindParam(':uid',$uid,PDO::PARAM_STR);
     $query->execute();
-    echo '<script>alert("Join Code has been changed")</script>';
+    echo "<script>alert('You have left the class');</script>";
+    echo "<script>window.location.href = 'manage-class.php'</script>";
   }
-
-  if (isset($_POST['new_attendance'])) {
-    $eid = $_GET['editid'];
-    $sql = "insert into tblattendance (class_id) values (:eid)";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':eid', $eid, PDO::PARAM_STR);
-    $query->execute();
-    echo '<script>alert("New attendance record has been created")</script>';
-}
-
-if (isset($_POST['delete_attendance'])) {
-  $aid = $_POST['attendance_id'];
-  $sql = "delete from tblattendance where ID=:aid";
-  $query = $dbh->prepare($sql);
-  $query->bindParam(':aid', $aid, PDO::PARAM_STR);
-  $query->execute();
-  echo '<script>alert("Attendance record has been deleted")</script>';
-}
 }
 ?>
 
@@ -125,9 +65,9 @@ if (isset($_POST['delete_attendance'])) {
               <?php
                   $uid = $_SESSION['sturecmsuid'];
                   $eid = $_GET['editid'];
-                  $sql = "SELECT * FROM tblclass, tblteacher WHERE teacher_id=:uid AND tblteacher.ID=:uid AND tblclass.ID=$eid";
+                  $sql = "SELECT * FROM tblclass, tblteacher WHERE tblclass.ID=:eid AND teacher_id=tblteacher.ID";
                   $query = $dbh->prepare($sql);
-                  $query->bindParam(':uid',$uid,PDO::PARAM_STR);
+                  $query->bindParam(':eid',$eid,PDO::PARAM_STR);
                   $query->execute();
                   $results = $query->fetchAll(PDO::FETCH_OBJ);
                   $cnt = 1; 
@@ -143,17 +83,17 @@ if (isset($_POST['delete_attendance'])) {
                              class="form-control" required='true', readonly>
                       </div>
                       <div class="form-group">
-                        <label for="exampleInputName1">Join Code</label>
-                        <input type="text" name="joincode"
-                             value="<?php echo htmlentities($row->JoinCode); ?>"
-                             class="form-control" required='true' readonly="">
+                        <label for="exampleInputName1">Teacher</label>
+                        <input type="text" name="teacher"
+                             value="<?php echo htmlentities($row->TeacherName); ?>"
+                             class="form-control" required='true', readonly>
+                      </div>
+                      <div class="text-right">
+                        <button type="submit" class="btn btn-primary mr-2" name="leave" style="background-color: red;">Leave Class</button>
                       </div>
                       <?php $cnt = $cnt + 1;
                     }
                   } ?>
-                  
-                  <button type="submit" class="btn btn-primary mr-2" name="regencode">Change Code
-                  </button>
                 </form>
               </div>
             </div>
@@ -174,9 +114,7 @@ if (isset($_POST['delete_attendance'])) {
                         <tr>
                           <th class="font-weight-bold">No.</th>
                           <th class="font-weight-bold">Time</th>
-                          <th class="font-weight-bold">Student</th>
-                          <th class="font-weight-bold"></th>
-                          <th class="font-weight-bold">Action</th>
+                          <th class="font-weight-bold">Status</th>
                         </tr>
                       </thead>
                       <tbody>
@@ -188,9 +126,11 @@ if (isset($_POST['delete_attendance'])) {
                         }
                         // Formula for pagination
                         $eid = $_GET['editid'];
-                        $sql = "SELECT tblattendance.* from tblattendance where class_id=:eid ORDER BY CreationTime ASC";
+                        $uid = $_SESSION['sturecmsuid'];
+                        $sql = "SELECT ID, CreationTime, sa.student_id FROM tblattendance LEFT JOIN (SELECT * FROM tblstudent_attendance WHERE student_id=:uid) sa ON sa.attendance_id=ID WHERE class_id=:eid ORDER BY CreationTime ASC;";
                         $query = $dbh->prepare($sql);
                         $query->bindParam(':eid',$eid,PDO::PARAM_STR);
+                        $query->bindParam(':uid',$uid,PDO::PARAM_STR);
                         $query->execute();
                         $results = $query->fetchAll(PDO::FETCH_OBJ);
 
@@ -203,28 +143,12 @@ if (isset($_POST['delete_attendance'])) {
                               <td><?php echo htmlentities($row->CreationTime); ?></td>
                               <td>
                                 <?php
-                                $aid = $row->ID;
-                                $sql1 = "SELECT * from tblstudent_attendance where attendance_id=:aid";
-                                $query1 = $dbh->prepare($sql1);
-                                $query1->bindParam(':aid', $aid, PDO::PARAM_STR);
-                                $query1->execute();
-                                $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
-                                $totalstudent = $query1->rowCount();
-                                echo htmlentities($totalstudent);
+                                if ($row->student_id != Null) {
+                                  echo "Attended";
+                                } else {
+                                  echo "Absent";
+                                }
                                 ?>
-                              </td>
-                              <td>
-                                <td >
-                                  <form class="forms-sample" method="post">
-                                    <input type="hidden" name="attendance_id" value="<?php echo htmlentities($aid); ?>">
-                                    <button type="submit" class="btn btn-sm btn-primary mr-2" style="background-color: gray; width: 100pt;" name="genqr">Gen QR</button>
-                                  </form>
-                                  <form class="forms-sample" method="post">
-                                    <input type="hidden" name="attendance_id" value="<?php echo htmlentities($aid); ?>">
-                                    <button type="submit" class="btn btn-sm btn-primary mr-2" name="delete_attendance" style="background-color: red; width: 100pt;">Delete</button>
-                                  </form>
-                                </td>
-                              </td>
                             </tr>
                         <?php $cnt = $cnt + 1;
                           }
@@ -232,20 +156,11 @@ if (isset($_POST['delete_attendance'])) {
                       </tbody>
                     </table>
                   </div>
-                  
-                    <div class="mt-4"></div>
-                    <form class="forms-sample" method="post">
-                  <button type="submit" class="btn btn-primary mr-2" name="new_attendance">New Attendance record</button>
-                  </form>
-                    
-
-                    
-                </div>
               </div>
             </div>
           </div>
       </div>
-        <div class="row" style="margin-left: 20px; ">
+        <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
           <div class="card-body">
@@ -260,17 +175,19 @@ if (isset($_POST['delete_attendance'])) {
               <th class="font-weight-bold">No.</th>
               <th class="font-weight-bold">Title</th>
               <th class="font-weight-bold">Start Time</th>
-              <th class="font-weight-bold">End Time</th>
               <th class="font-weight-bold">Submitted</th>
-              <th class="font-weight-bold">Average Score</th>
+              <th class="font-weight-bold">Point</th>
             </tr>
                 </thead>
                 <tbody>
             <?php
 
-            $sql = "SELECT tbltest.* from tbltest where class_id=:eid ORDER BY StartTime DESC";
+            $eid = $_GET['editid'];
+            $uid = $_SESSION['sturecmsuid'];
+            $sql = "SELECT t.*, st.SubmitTime, st.TotalPoint from tbltest t LEFT JOIN (SELECT * FROM tblstudent_test WHERE student_id=:uid) st ON test_id=ID WHERE class_id=:eid ORDER BY StartTime ASC;";
             $query = $dbh->prepare($sql);
             $query->bindParam(':eid',$eid,PDO::PARAM_STR);
+            $query->bindParam(':uid',$uid,PDO::PARAM_STR);
             $query->execute();
             $results = $query->fetchAll(PDO::FETCH_OBJ);
 
@@ -282,34 +199,16 @@ if (isset($_POST['delete_attendance'])) {
                   <td><?php echo htmlentities($cnt); ?></td>
                   <td><?php echo htmlentities($row->TestName); ?></td>
                   <td><?php echo htmlentities($row->StartTime); ?></td>
-                  <td><?php echo htmlentities($row->EndTime); ?></td>
-                  <td>
-              <?php
-              $tid = $row->ID;
-              $sql1 = "SELECT * from tblstudent_test where test_id=:tid and SubmitTime!=Null";
-              $query1 = $dbh->prepare($sql1);
-              $query1->bindParam(':tid', $tid, PDO::PARAM_STR);
-              $query1->execute();
-              $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
-              $totalstudent = $query1->rowCount();
-              echo htmlentities($totalstudent);
-              ?>
-                  </td>
-                  <td>
-                  <?php
-                  $tid = $row->ID;
-                  $sql1 = "SELECT ROUND(AVG(TotalPoint),2) as average from tblstudent_test where test_id=:tid and SubmitTime!=Null";
-                  $query1 = $dbh->prepare($sql1);
-                  $query1->bindParam(':tid', $tid, PDO::PARAM_STR);
-                  $query1->execute();
-                  $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
-                  if ($results1[0]->average != Null) {
-                    echo htmlentities($results1[0]->average);
-                  } else {
+                  <td><?php if ($row->SubmitTime!=Null) {
+                    echo htmlentities($row->SubmitTime);
+                   } else {
                     echo "N/A";
-                  }
-              ?>
-                  </td>
+                   } ?></td>
+                  <td><?php if ($row->TotalPoint!=Null) {
+                    echo htmlentities($row->TotalPoint);
+                   } else {
+                    echo "N/A";
+                   } ?></td>
                 </tr>
             <?php $cnt = $cnt + 1;
               }
