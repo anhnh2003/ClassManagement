72c2fc505e7d818d00d6618f2a7403df08acff89
anhnh2003 <anh.nhcoder@gmail.com>
2024-05-19 10:53:17 +0700

    add token to manage-class and fix bug

--- teacher/manage-class.php
+++ teacher/manage-class.php
@@ -2,7 +2,8 @@
 session_start();
 include('includes/dbconnection.php');
 // Check if the user is logged in and the session variables are set
-if (strlen($_SESSION['sturecmsstuid']) == 0) {
+$_SESSION['sturecmstuid'] = $_SESSION['sturecmsstuid'];
+if (strlen($_SESSION['sturecmstuid']) == 0) {
 
   header('location:logout.php');
   exit();
@@ -46,7 +47,27 @@ if (strlen($_SESSION['sturecmsstuid']) == 0) {
   <!-- plugins:css -->
   <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
   <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
-@@ -52,11 +52,11 @@
+  <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="./vendors/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="./vendors/chartist/chartist.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="./css/style.css">
+  <!-- End layout styles -->
+</head>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <?php include_once('includes/header.php'); ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <?php include_once('includes/sidebar.php'); ?>
+      <!-- partial -->
       <div class="main-panel">
         <div class="content-wrapper">
           <div class="page-header">
@@ -60,7 +81,8 @@ if (strlen($_SESSION['sturecmsstuid']) == 0) {
               </ol>
             </nav>
           </div>
-@@ -65,7 +65,7 @@
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
                   <div class="d-sm-flex align-items-center mb-4">
@@ -69,7 +91,9 @@ if (strlen($_SESSION['sturecmsstuid']) == 0) {
                     <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> View all Classes</a>
                   </div>
                   <div class="table-responsive border rounded p-1">
-@@ -75,6 +75,9 @@
+                    <table class="table">
+                      <thead>
+                        <tr>
                           <th class="font-weight-bold">No.</th>
                           <th class="font-weight-bold">Name</th>
                           <th class="font-weight-bold">Code</th>
@@ -79,7 +103,33 @@ if (strlen($_SESSION['sturecmsstuid']) == 0) {
                           <th class="font-weight-bold">Action</th>
                         </tr>
                       </thead>
-@@ -109,6 +112,39 @@
+                      <tbody>
+                        <?php
+                        if (isset($_GET['pageno'])) {
+                          $pageno = $_GET['pageno'];
+                        } else {
+                          $pageno = 1;
+                        }
+                        // Formula for pagination
+                        $uid= $_SESSION['sturecmsuid'];
+                        $no_of_records_per_page = 15;
+                        $offset = ($pageno - 1) * $no_of_records_per_page;
+                        $ret = "SELECT ID FROM tblclass";
+                        $query1 = $dbh->prepare($ret);
+                        $query1->execute();
+                        $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                        $total_rows = $query1->rowCount();
+                        $total_pages = ceil($total_rows / $no_of_records_per_page);
+                        $sql = "SELECT tblclass.* from tblclass where teacher_id=:uid ORDER BY tblclass.CreationTime DESC LIMIT $offset, $no_of_records_per_page";
+                        $query = $dbh->prepare($sql);
+                        $query->bindParam(':uid',$uid,PDO::PARAM_STR);
+                        $query->execute();
+                        $results = $query->fetchAll(PDO::FETCH_OBJ);
+                        $cnt = 1;
+                        if ($query->rowCount() > 0) {
+                          foreach ($results as $row) {
+                        ?>
+                            <tr>
                               <td><?php echo htmlentities($cnt); ?></td>
                               <td><?php echo htmlentities($row->ClassName); ?></td>
                               <td><?php echo htmlentities($row->JoinCode); ?></td>
@@ -119,3 +169,67 @@ if (strlen($_SESSION['sturecmsstuid']) == 0) {
                               <td>
                                 <div><a href="class-detail.php?editid=<?php echo htmlentities($row->ID); ?>"><i class="icon-eye"></i></a></div>
                               </td>
+                            </tr>
+                        <?php $cnt = $cnt + 1;
+                          }
+                        } ?>
+                      </tbody>
+                    </table>
+                  </div>
+                  <div align="left">
+                    <ul class="pagination">
+                      <li><a href="?pageno=1"><strong>First></strong></a></li>
+                      <li class="<?php if ($pageno <= 1) {
+                              echo 'disabled';
+                            } ?>">
+                        <a href="<?php if ($pageno <= 1) {
+                                echo '#';
+                              } else {
+                                echo "?pageno=" . ($pageno - 1);
+                              } ?>"><strong style="padding-left: 10px">Prev></strong></a>
+                      </li>
+                      <li class="<?php if ($pageno >= $total_pages) {
+                              echo 'disabled';
+                            } ?>">
+                        <a href="<?php if ($pageno >= $total_pages) {
+                                echo '#';
+                              } else {
+                                echo "?pageno=" . ($pageno + 1);
+                              } ?>"><strong style="padding-left: 10px">Next></strong></a>
+                      </li>
+                      <li><a href="?pageno=<?php echo $total_pages; ?>"><strong style="padding-left: 10px">Last</strong></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <?php include_once('includes/footer.php'); ?>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="./vendors/chart.js/Chart.min.js"></script>
+  <script src="./vendors/moment/moment.min.js"></script>
+  <script src="./vendors/daterangepicker/daterangepicker.js"></script>
+  <script src="./vendors/chartist/chartist.min.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/misc.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page -->
+  <script src="./js/dashboard.js"></script>
+  <!-- End custom js for this page -->
+</body>
+</html>
