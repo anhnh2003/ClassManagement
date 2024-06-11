c9299adeacae40248607ef719288a6031329459f
Hwi <84950295+qhwi@users.noreply.github.com>
2024-06-11 14:30:15 +0700

    update student/check-attendance
    now auto scan QR without reloading, add error messages

--- student/class-detail.php
+++ student/class-detail.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//error_reporting(0);
+error_reporting(0);
 include('includes/dbconnection.php');
 
 if (strlen($_SESSION['sturecmsuid']) == 0) {
@@ -10,64 +10,29 @@ if (strlen($_SESSION['sturecmsuid']) == 0) {
   $uid = $_SESSION['sturecmsuid'];
   $sql = "SELECT * FROM tblstudent_class WHERE student_id=:uid AND class_id=:eid";
   $query = $dbh->prepare($sql);
-  $query->bindParam(':eid',$eid,PDO::PARAM_STR);
-  $query->bindParam(':uid',$uid,PDO::PARAM_STR);
+  $query->bindParam(':eid', $eid, PDO::PARAM_STR);
+  $query->bindParam(':uid', $uid, PDO::PARAM_STR);
   $query->execute();
   if ($query->rowCount() == 0) {
     header('location:manage-class.php');
     exit();
   }
-  
+
   if (isset($_POST['leave'])) {
     $sql = "DELETE FROM tblstudent_class WHERE student_id=:uid AND class_id=:eid";
     $query = $dbh->prepare($sql);
-    $query->bindParam(':eid',$eid,PDO::PARAM_STR);
-    $query->bindParam(':uid',$uid,PDO::PARAM_STR);
+    $query->bindParam(':eid', $eid, PDO::PARAM_STR);
+    $query->bindParam(':uid', $uid, PDO::PARAM_STR);
     $query->execute();
     echo "<script>alert('You have left the class');</script>";
     echo "<script>window.location.href = 'manage-class.php'</script>";
   }
 }
-
-
-$filePath = __DIR__ . "/temp/photo.png";
-if (file_exists($filePath)) {
-    require __DIR__ . "/../vendor/autoload.php";
-    $qrcode = new Zxing\QrReader($filePath);
-    $text = $qrcode->text();
-    unlink('temp/photo.png');
-}
-// exit;
-
-$sql = "SELECT ID, LastGeneratedTime, TimeToLive FROM tblattendance WHERE Secret=:text";
-$query = $dbh->prepare($sql);
-$query->bindParam(':text',$text,PDO::PARAM_STR);
-$query->execute();
-$result = $query->fetch(PDO::FETCH_OBJ);
-if ($query->rowCount() == 1) {
-  date_default_timezone_set('Asia/Ho_Chi_Minh');
-  $current_time = date('Y-m-d H:i:s');
-  $last_generated_time = strtotime($result->LastGeneratedTime);
-  $time_difference = strtotime($current_time) - $last_generated_time;
-  if ($time_difference > $result->TimeToLive) {
-    echo "<script>alert('QR code expired');</script>";
-  } else {
-    $aid = $result->ID;
-
-    $sql = "INSERT INTO tblstudent_attendance (student_id, attendance_id) VALUES (:uid, :aid)";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':uid',$uid,PDO::PARAM_STR);
-    $query->bindParam(':aid',$aid,PDO::PARAM_STR);
-    $query->execute();
-    echo "<script>alert('Attendance recorded');</script>";
-  }
-  echo "<script>window.location.href = 'class-detail.php?editid=$eid'</script>";
-  die;
-}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>Student Management System || Manage Class</title>
   <!-- plugins:css -->
@@ -82,71 +47,68 @@ if ($query->rowCount() == 1) {
   <!-- inject:css -->
   <!-- endinject -->
   <!-- Layout styles -->
-  <link rel="stylesheet" href="css/style.css"/>
+  <link rel="stylesheet" href="css/style.css" />
 </head>
+
 <body>
-<div class="container-scroller">
-  <!-- partial:partials/_navbar.html -->
-  <?php include_once('includes/header.php'); ?>
-  <!-- partial -->
-  <div class="container-fluid page-body-wrapper">
-    <!-- partial:partials/_sidebar.html -->
-    <?php include_once('includes/sidebar.php'); ?>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <?php include_once('includes/header.php'); ?>
     <!-- partial -->
-    <div class="main-panel">
-      <div class="content-wrapper">
-        <div class="page-header">
-          <h3 class="page-title"> Manage Class </h3>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-              <li class="breadcrumb-item active" aria-current="page"> Manage Class</li>
-            </ol>
-          </nav>
-        </div>
-        <div class="row">
-          <div class="col-12 grid-margin stretch-card">
-            <div class="card">
-              <div class="card-body">
-              <?php
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <?php include_once('includes/sidebar.php'); ?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="page-header">
+            <h3 class="page-title"> Manage Class </h3>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
+                <li class="breadcrumb-item active" aria-current="page"> Manage Class</li>
+              </ol>
+            </nav>
+          </div>
+          <div class="row">
+            <div class="col-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <?php
                   $uid = $_SESSION['sturecmsuid'];
                   $eid = $_GET['editid'];
                   $sql = "SELECT * FROM tblclass, tblteacher WHERE tblclass.ID=:eid AND teacher_id=tblteacher.ID";
                   $query = $dbh->prepare($sql);
-                  $query->bindParam(':eid',$eid,PDO::PARAM_STR);
+                  $query->bindParam(':eid', $eid, PDO::PARAM_STR);
                   $query->execute();
                   $results = $query->fetchAll(PDO::FETCH_OBJ);
-                  $cnt = 1; 
+                  $cnt = 1;
                   if ($query->rowCount() > 0) {
                     foreach ($results as $row) {
-                      ?>
-                <h4 class="card-title" style="text-align: center;"> <?php echo htmlentities($row->ClassName); ?> </h4>
-                <form class="forms-sample" method="post">
-                      <div class="form-group">
-                        <label for="exampleInputName1">Room</label>
-                        <input type="text" name="room"
-                             value="<?php echo htmlentities($row->Room); ?>"
-                             class="form-control" required='true', readonly>
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputName1">Teacher</label>
-                        <input type="text" name="teacher"
-                             value="<?php echo htmlentities($row->TeacherName); ?>"
-                             class="form-control" required='true', readonly>
-                      </div>
-                      <div class="text-right">
-                        <button type="submit" class="btn btn-primary mr-2" name="leave" style="background-color: red; border-color: red;">Leave Class</button>
-                      </div>
-                      <?php $cnt = $cnt + 1;
+                  ?>
+                      <h4 class="card-title" style="text-align: center;"> <?php echo htmlentities($row->ClassName); ?> </h4>
+                      <form class="forms-sample" method="post">
+                        <div class="form-group">
+                          <label for="exampleInputName1">Room</label>
+                          <input type="text" name="room" value="<?php echo htmlentities($row->Room); ?>" class="form-control" required='true' readonly>
+                        </div>
+                        <div class="form-group">
+                          <label for="exampleInputName1">Teacher</label>
+                          <input type="text" name="teacher" value="<?php echo htmlentities($row->TeacherName); ?>" class="form-control" required='true' readonly>
+                        </div>
+                        <div class="text-right">
+                          <button type="submit" class="btn btn-primary mr-2" name="leave" style="background-color: red; border-color: red;">Leave Class</button>
+                        </div>
+                        <?php $cnt = $cnt + 1;
                     }
                   } ?>
-                </form>
+                      </form>
+                </div>
               </div>
+
             </div>
-            
           </div>
-        </div>
-        <div class="row">
+          <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
@@ -175,8 +137,8 @@ if ($query->rowCount() == 1) {
                         $uid = $_SESSION['sturecmsuid'];
                         $sql = "SELECT ID, CreationTime, sa.student_id FROM tblattendance LEFT JOIN (SELECT * FROM tblstudent_attendance WHERE student_id=:uid) sa ON sa.attendance_id=ID WHERE class_id=:eid ORDER BY CreationTime ASC;";
                         $query = $dbh->prepare($sql);
-                        $query->bindParam(':eid',$eid,PDO::PARAM_STR);
-                        $query->bindParam(':uid',$uid,PDO::PARAM_STR);
+                        $query->bindParam(':eid', $eid, PDO::PARAM_STR);
+                        $query->bindParam(':uid', $uid, PDO::PARAM_STR);
                         $query->execute();
                         $results = $query->fetchAll(PDO::FETCH_OBJ);
 
@@ -190,11 +152,12 @@ if ($query->rowCount() == 1) {
                               <td>
                                 <?php
                                 if ($row->student_id != Null) {
-                                  echo "Attended";
+                                  echo '<label class="badge badge-success"style="font-size: 13px;">Attended</label>';
                                 } else {
-                                  echo "Absent";
+                                  echo '<label class="badge badge-danger" style="font-size: 13px;">Absent</label>';
                                 }
                                 ?>
+                              </td>
                             </tr>
                         <?php $cnt = $cnt + 1;
                           }
@@ -202,97 +165,98 @@ if ($query->rowCount() == 1) {
                       </tbody>
                     </table>
                   </div>
+                </div>
               </div>
             </div>
           </div>
-      </div>
-        <div class="row">
+          <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
-          <div class="card-body">
-            <div class="d-sm-flex align-items-center mb-4">
-              <h4 class="card-title mb-sm-0">Tests</h4>
-              <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> </a>
-            </div>
-            <div class="table-responsive border rounded p-1">
-              <table class="table">
-                <thead>
-            <tr>
-              <th class="font-weight-bold">No.</th>
-              <th class="font-weight-bold">Title</th>
-              <th class="font-weight-bold">Start Time</th>
-              <th class="font-weight-bold">Submitted</th>
-              <th class="font-weight-bold">Point</th>
-            </tr>
-                </thead>
-                <tbody>
-            <?php
+                <div class="card-body">
+                  <div class="d-sm-flex align-items-center mb-4">
+                    <h4 class="card-title mb-sm-0">Tests</h4>
+                    <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> </a>
+                  </div>
+                  <div class="table-responsive border rounded p-1">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th class="font-weight-bold">No.</th>
+                          <th class="font-weight-bold">Title</th>
+                          <th class="font-weight-bold">Start Time</th>
+                          <th class="font-weight-bold">Submitted</th>
+                          <th class="font-weight-bold">Point</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php
 
-            $eid = $_GET['editid'];
-            $uid = $_SESSION['sturecmsuid'];
-            $sql = "SELECT t.*, st.SubmitTime, st.TotalPoint from tbltest t LEFT JOIN (SELECT * FROM tblstudent_test WHERE student_id=:uid) st ON test_id=ID WHERE class_id=:eid ORDER BY StartTime ASC;";
-            $query = $dbh->prepare($sql);
-            $query->bindParam(':eid',$eid,PDO::PARAM_STR);
-            $query->bindParam(':uid',$uid,PDO::PARAM_STR);
-            $query->execute();
-            $results = $query->fetchAll(PDO::FETCH_OBJ);
+                        $eid = $_GET['editid'];
+                        $uid = $_SESSION['sturecmsuid'];
+                        $sql = "SELECT t.*, st.SubmitTime, st.TotalPoint from tbltest t LEFT JOIN (SELECT * FROM tblstudent_test WHERE student_id=:uid) st ON test_id=ID WHERE class_id=:eid ORDER BY StartTime ASC;";
+                        $query = $dbh->prepare($sql);
+                        $query->bindParam(':eid', $eid, PDO::PARAM_STR);
+                        $query->bindParam(':uid', $uid, PDO::PARAM_STR);
+                        $query->execute();
+                        $results = $query->fetchAll(PDO::FETCH_OBJ);
 
-            $cnt = 1;
-            if ($query->rowCount() > 0) {
-              foreach ($results as $row) {
-            ?>
-                <tr>
-                  <td><?php echo htmlentities($cnt); ?></td>
-                  <td><a href="test-detail.php?editid=<?php echo htmlentities($row->ID); ?>"><?php echo htmlentities($row->TestName); ?></a></td>
-                  <td><?php echo htmlentities($row->StartTime); ?></td>
-                  <td><?php if ($row->SubmitTime!=Null) {
-                    echo htmlentities($row->SubmitTime);
-                   } else {
-                    echo "N/A";
-                   } ?></td>
-                  <td><?php if ($row->TotalPoint!=Null) {
-                    echo htmlentities($row->TotalPoint);
-                   } else {
-                    echo "N/A";
-                   } ?></td>
-                </tr>
-            <?php $cnt = $cnt + 1;
-              }
-            } ?>
-                </tbody>
-              </table>
-            </div>
+                        $cnt = 1;
+                        if ($query->rowCount() > 0) {
+                          foreach ($results as $row) {
+                        ?>
+                            <tr>
+                              <td><?php echo htmlentities($cnt); ?></td>
+                              <td><a href="test-detail.php?editid=<?php echo htmlentities($row->ID); ?>"><?php echo htmlentities($row->TestName); ?></a></td>
+                              <td><?php echo htmlentities($row->StartTime); ?></td>
+                              <td><?php if ($row->SubmitTime != Null) {
+                                    echo htmlentities($row->SubmitTime);
+                                  } else {
+                                    echo "N/A";
+                                  } ?></td>
+                              <td><?php if ($row->TotalPoint != Null) {
+                                    echo htmlentities($row->TotalPoint);
+                                  } else {
+                                    echo "N/A";
+                                  } ?></td>
+                            </tr>
+                        <?php $cnt = $cnt + 1;
+                          }
+                        } ?>
+                      </tbody>
+                    </table>
+                  </div>
 
-              
-          </div>
+
+                </div>
               </div>
             </div>
           </div>
-            </div>
-      <!-- content-wrapper ends -->
-      <!-- partial:partials/_footer.html -->
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
 
-      <!-- partial -->
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
     </div>
-    <!-- main-panel ends -->
+    <!-- page-body-wrapper ends -->
   </div>
-  <!-- page-body-wrapper ends -->
-</div>
-<!-- container-scroller -->
-<!-- plugins:js -->
-<script src="vendors/js/vendor.bundle.base.js"></script>
-<!-- endinject -->
-<!-- Plugin js for this page -->
-<script src="vendors/select2/select2.min.js"></script>
-<script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="js/off-canvas.js"></script>
-<script src="js/misc.js"></script>
-<!-- endinject -->
-<!-- Custom js for this page -->
-<script src="js/typeahead.js"></script>
-<script src="js/select2.js"></script>
-<!-- End custom js for this page -->
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="vendors/select2/select2.min.js"></script>
+  <script src="vendors/typeahead.js/typeahead.bundle.min.js"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/misc.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page -->
+  <script src="js/typeahead.js"></script>
+  <script src="js/select2.js"></script>
+  <!-- End custom js for this page -->
 </body>
+
 </html>
---
+++ student/scanQR.php
@@ -0,0 +1,70 @@
+<?php
+include('includes/dbconnection.php');
+
+if (isset($_POST['imgData'])) {
+    $imgData = $_POST['imgData'];
+    $uid = $_POST['uid'];
+
+    // Remove the header of the data URL
+    $imgData = str_replace('data:image/png;base64,', '', $imgData);
+    $imgData = str_replace(' ', '+', $imgData);
+
+    // Decode the base64 data
+    $imgData = base64_decode($imgData);
+
+    // Write the data to a file in the student/temp directory
+    if (!is_dir("temp")) {
+        mkdir("temp");
+    }
+    $filePath = "temp/photo".rand(100000,999999).".png";
+    file_put_contents($filePath, $imgData);
+    
+    // Decode to text
+    require "../vendor/autoload.php";
+    $qrcode = new Zxing\QrReader($filePath);
+    $text = $qrcode->text();
+    unlink($filePath);
+
+    // Check if the QR code is valid
+    if ($text) {
+        $sql = "SELECT ID, LastGeneratedTime, TimeToLive FROM tblattendance WHERE Secret=:text";
+        $query = $dbh->prepare($sql);
+        $query->bindParam(':text', $text, PDO::PARAM_STR);
+        $query->execute();
+        $result = $query->fetch(PDO::FETCH_OBJ);
+
+        if ($query->rowCount() == 1) {
+            date_default_timezone_set('Asia/Ho_Chi_Minh');
+            $current_time = date('Y-m-d H:i:s');
+            $last_generated_time = strtotime($result->LastGeneratedTime);
+            $time_difference = strtotime($current_time) - $last_generated_time;
+
+            if ($time_difference > $result->TimeToLive) {
+                echo -1;
+            } else {
+                $aid = $result->ID;
+                $sql = "SELECT * FROM tblstudent_attendance WHERE student_id = :uid AND attendance_id = :aid";
+                $query = $dbh->prepare($sql);
+                $query->bindParam(':uid', $uid, PDO::PARAM_STR);
+                $query->bindParam(':aid', $aid, PDO::PARAM_STR);
+                $query->execute();
+
+                if ($query->rowCount() > 0) {
+                    echo -2;
+                } else {
+                    $sql = "INSERT INTO tblstudent_attendance(student_id, attendance_id) VALUES(:uid, :aid)";
+                    $query = $dbh->prepare($sql);
+                    $query->bindParam(':uid', $uid, PDO::PARAM_STR);
+                    $query->bindParam(':aid', $aid, PDO::PARAM_STR);
+                    $query->execute();
+                    echo 1;
+                }
+            }
+        } else {
+            echo -1;
+        }
+    } else {
+        echo 0;
+    }
+}
+?>
